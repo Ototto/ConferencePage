61fbad8734ec0c37e52903a43610f666c24099f4
Mariusz Kwiatek Laptop <mariuszkwiatek@outlook.com>
2017-04-26 00:53:40 +0200

    Navbar, contact, description, button, whole layout

--- js/homeController.js
+++ js/homeController.js
@@ -1,10 +1,34 @@
-var HomeController = new function(){
+var HomeController = new function () {
 
-    var init = new function(){
-        
+    var navbarControl = new function(){
+        $(window).scroll(function () {
+            var screenHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0) - 60;
+            if ($(window).scrollTop() >= screenHeight) {
+                $('.navbarBar').css({
+                    'background-color': 'rgba(26, 26, 255, 0.9)',
+                    'transition' : '0.3s',       
+                });
+
+                $('.navbarNavigation').css({
+                     'padding' : '8px'
+                });
+
+                $('.site-logo').css({
+                     'margin-top' : '8px'
+                });
+
+            } else {
+                $('.navbarBar, .navbarNavigation, .site-logo').attr('style', '');
+            }
+        });
     }
 
-    return{
+    
+    var init = new function () {
+        navbarControl;
+    }
+
+    return {
         init: init
     }
 }
