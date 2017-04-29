d0354e35bb27df88e2ce7f67a2be6f70849559f6
Mariusz Kwiatek Laptop <mariuszkwiatek@outlook.com>
2017-04-30 00:52:35 +0200

    Navbar for mobile and footer

--- js/homeController.js
+++ js/homeController.js
@@ -1,20 +1,20 @@
 var HomeController = new function () {
 
-    var navbarControl = new function(){
+    var navbarControl = new function () {
         $(window).scroll(function () {
             var screenHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0) - 60;
             if ($(window).scrollTop() >= screenHeight) {
                 $('.navbarBar').css({
                     'background-color': 'rgba(26, 26, 255, 0.9)',
-                    'transition' : '0.3s',       
+                    'transition': '0.3s',
                 });
 
                 $('.navbarNavigationList').css({
-                     'padding' : '8px'
+                    'padding': '8px'
                 });
 
                 $('.site-logo').css({
-                     'margin-top' : '8px'
+                    'margin-top': '8px'
                 });
 
             } else {
@@ -23,10 +23,29 @@ var HomeController = new function () {
         });
     }
 
-    
+    var closeMobileNavbar = function(){
+        $('.navbarNavigation').attr('style', '');
+    }
+
+    var openMobileNavbar = function(){
+        $('.navbarNavigation').css({
+            'display': 'block'
+        });
+    }
+
+    var mobileNavbarControl = function(){
+        if ($('.navbarNavigation').css('display') == 'none') {
+            openMobileNavbar();
+        }
+        else {
+            closeMobileNavbar();
+        }
+    }
+
     var init = new function () {
         navbarControl;
-        
+        $('#menuBtn').click(mobileNavbarControl);
+        $('.navigationLink').click(closeMobileNavbar);
     }
 
     return {
